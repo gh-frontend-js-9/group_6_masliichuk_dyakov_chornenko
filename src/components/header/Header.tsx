01890cf222754d4536b82d090ceaf8824fda3798
Olechka <olha_nedopaka@ukr.net>
2020-03-18 21:00:43 +0200

    media queries for header added

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { SyntheticEvent } from 'react';
 import logo from './../../assets/images/header/logo.png';
 import search from './../../assets/images/header/search-icon.png';
 
+interface IProps {}
 
-export default class Header extends React.Component {
+interface IState {
+    isOpen: boolean;
+}
+
+export default class Header extends React.Component<IProps, IState> {
+
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            isOpen: false
+        };
+    }
+
+    handleClick = (e: SyntheticEvent) => {
+        e.preventDefault();
+        if (this.state.isOpen) {
+            this.setState({isOpen: false});
+        } else {
+            this.setState({isOpen: true});
+        }
+    }
+
     render() {
         return (
-            <div className="container header-container">
-                <header className="header">
+            <header className="container header-container">
+                <div className="header">
                     <div className="header__left">
                         <div className="header__logo">
                             <a href="#"><img src={logo} alt="logo" /></a>
                         </div>
 
-                        <div className="header__nav">
+                        <div className={this.state.isOpen ? 'header__nav header__nav--open' : 'header__nav'}>
                             <ul>
                                 <li><a href="">Popular</a></li>
                                 <li><a href="">New</a></li>
@@ -29,10 +51,20 @@ export default class Header extends React.Component {
                         <a href="#"><img src={search} alt="search" /></a>
                     </div>
 
+                    <div className="header__toggle">
+                        <a href="#" onClick={this.handleClick} >
+                            <div className="header__hamburger" >
+                                <div></div>
+                                <div></div>
+                                <div></div>
+                            </div>
+                        </a>
+                    </div>
 
-                </header>
 
-            </div>
+                </div>
+
+            </header>
         )
 
     }
