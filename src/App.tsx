32d910bfb2f869e480091f44d76d638c24bb1070
Olechka <olha_nedopaka@ukr.net>
2020-04-20 22:01:02 +0300

    fixes added

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,18 @@ import Header from './components/header/Header';
 import Hero from './components/hero/Hero';
 import {Footer} from "./components/footer";
 import {BlogNews} from "./components/blognews";
+import Cards from './components/cards/Cards';
+
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Hero/>
+      <Cards/>
       <BlogNews/>
       <Footer/>
+      
     </div>
   );
 }
